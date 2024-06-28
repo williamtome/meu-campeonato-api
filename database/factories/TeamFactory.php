6a60b62d759017656dd7b8a2dfabb259252476b3
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 15:38:10 -0300

    Implementado factory de times para ajudar nos testes

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
+class TeamFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name,
+            'points' => 0,
+        ];
+    }
+}
