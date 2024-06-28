e5a9f929a9902b68f36f484e406ce0a8b68e8fa3
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 15:28:41 -0300

    Criado rota para iniciar as fases do jogo

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\RoundController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('teams', TeamController::class);
+
+Route::post('/rounds/{id}/start', [RoundController::class, 'start']);
