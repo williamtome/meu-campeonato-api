d2c1eb1fff630ce044df1dda0bcf819b38a6ecdf
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 18:16:47 -0300

    Implementado melhorias de código no service das rodadas

--- app/Services/RoundService.php
+++ app/Services/RoundService.php
@@ -11,37 +11,25 @@ class RoundService
 {
     public function startQuarterfinals(Round $round)
     {
-        if (Team::greaterThanEightTeams()) {
-            abort(400, 'Não é permitido iniciar a fase com mais de 8 times. Por favor, remova algum time.');
-        }
-
-        if (Team::lessThanEightTeams()) {
-            abort(400, 'Não é permitido iniciar a fase com menos de 8 times. Por favor, adicione mais times.');
-        }
+        abort_if($round->isStarted(), 400, 'As quartas de final já foram iniciadas.');
+        abort_if($round->isFinished(), 400, 'As quartas de final foram finalizadas.');
+        abort_if(Team::greaterThanEightTeams(),400, 'Não é permitido iniciar a fase com mais de 8 times. Por favor, remova algum time.');
+        abort_if(Team::lessThanEightTeams(), 400, 'Não é permitido iniciar a fase com menos de 8 times. Por favor, adicione mais times.');
 
         $shuffledTeams = Team::all()->shuffle()->all();
 
-        try {
-            DB::beginTransaction();
+        for ($i = 0; $i < Team::count(); $i++) {
+            $team = $shuffledTeams[$i];
+            $nextTeam = $shuffledTeams[++$i];
 
-            for ($i = 0; $i < Team::count(); $i++) {
-                $team = $shuffledTeams[$i];
-                $nextTeam = $shuffledTeams[++$i];
-
-                MatchGame::create([
-                    'round_id' => $round->id,
-                    'team1_id' => $team->id,
-                    'team2_id' => $nextTeam->id,
-                    'match_date' => now()->addDay()->toDateTimeString()
-                ]);
-            }
-
-            $round->update(['started' => true]);
-
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
+            MatchGame::create([
+                'round_id' => $round->id,
+                'team1_id' => $team->id,
+                'team2_id' => $nextTeam->id,
+                'match_date' => now()->addDay()->toDateTimeString()
+            ]);
         }
+
+        $round->update(['started' => true]);
     }
 }
