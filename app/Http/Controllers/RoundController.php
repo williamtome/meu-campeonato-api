92302855b82a07d18f0f0202622df0a2c19c7da4
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 18:20:10 -0300

    Implementado método de simulação das partidas da fase utilizando service de partidas

--- app/Http/Controllers/RoundController.php
+++ app/Http/Controllers/RoundController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Round;
+use App\Services\MatchGameService;
 use App\Services\RoundService;
+use Illuminate\Support\Facades\DB;
 
 class RoundController extends Controller
 {
-    public function __construct(private RoundService $roundService)
-    { }
+    public function __construct(
+        private RoundService $roundService,
+        private MatchGameService $matchGameService
+    ) { }
+
+    public function matches(int $roundId)
+    {
+        $round = Round::findOrFail($roundId);
+
+        return response()->json($round->matches);
+    }
 
     public function start(int $roundId)
     {
@@ -21,5 +32,27 @@ class RoundController extends Controller
         return response()->json([
             'message' => 'Etapa das qurtas de final iniciada com sucesso.'
         ]);
+    }
+
+    public function simulate(int $roundId)
+    {
+        $round = Round::findOrFail($roundId);
+
+        DB::beginTransaction();
+
+        try {
+            foreach ($round->matches as $match) {
+                $this->matchGameService->simulate($match);
+            }
+
+            $round->update(['finished' => true]);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+
+        return response()->json(['message' => 'Simulação das partidas da(s) ' . $round->name . ' finalizadas com sucesso.']);
     }
 }
