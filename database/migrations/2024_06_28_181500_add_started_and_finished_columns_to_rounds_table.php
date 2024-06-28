a99f7c8a4ed07016a850bc50d2d57eb7d48f958b
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 15:40:13 -0300

    Adicionado colunas na tabela de fases para indicar se elas foram iniciadas e/ou finalizadas

---
+++ database/migrations/2024_06_28_181500_add_started_and_finished_columns_to_rounds_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rounds', function (Blueprint $table) {
+            $table->boolean('started')->after('name')->default(false);
+            $table->boolean('finished')->after('started')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rounds', function (Blueprint $table) {
+            $table->dropColumn(['started', 'finished']);
+        });
+    }
+};
