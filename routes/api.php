0dbb3ade98075cbbdd7793538275e7615200ab8f
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 18:14:57 -0300

    Criadas rotas de api para as rodadas

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\MatchGameController;
 use App\Http\Controllers\RoundController;
 use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('teams', TeamController::class);
 
-Route::post('/rounds/{id}/start', [RoundController::class, 'start']);
+Route::prefix('/rounds')->group(function () {
+    Route::post('/{id}/start', [RoundController::class, 'start']);
+    Route::get('/{id}/matches', [RoundController::class, 'matches']);
+    Route::post('/{id}/matches/simulate', [RoundController::class, 'simulate']);
+});
