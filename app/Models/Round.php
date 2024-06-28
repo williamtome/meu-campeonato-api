6a129aea0d9f84d0f28999a08dc7794a785e173e
William Tomé <william.weirich@helioprint.com.br>
2024-06-28 15:39:08 -0300

    Implementado metodo para retornar o nome da função de acordo com o id da fase a ser iniciada

--- app/Models/Round.php
+++ app/Models/Round.php
@@ -12,6 +12,21 @@ class Round extends Model
 
     protected $fillable = ['name'];
 
+    public const QUARTER_FINALS = 1;
+    public const SEMIFINAL = 2;
+    public const FINAL = 3;
+
+    public function getRoundFunctionName()
+    {
+        $rounds = [
+            self::QUARTER_FINALS => 'startQuarterfinals',
+            self::SEMIFINAL => 'startSemifinal',
+            self::FINAL => 'startFinal',
+        ];
+
+        return data_get($rounds, $this->id);
+    }
+
     public function matches(): HasMany
     {
         return $this->hasMany(MatchGame::class);
